e55ff96c054a3e2f09161e429cbf9e95e99307fc
NikitaBashiroff <164854801+NikitaBashiroff@users.noreply.github.com>
2026-08-30 20:01:32 +0500

    Update analytics.js

--- resources/analytics.js
+++ resources/analytics.js
@@ -2,16 +2,6 @@
     m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();
     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112014818', 'ym');
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112089026', 'ym');
 
-ym(112014818, 'init', {
-    ssr: true, 
-    webvisor: true, 
-    clickmap: true, 
-    ecommerce: "dataLayer", 
-    referrer: document.referrer, 
-    url: location.href, 
-    accurateTrackBounce: true, 
-    trackLinks: true
-});
+ym(112089026, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
